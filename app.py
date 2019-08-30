ce48a57054d412dca9d8e3c4e9edc341781b0fb1
Bruno <bdelafontaine92@gmail.com>
2019-08-30 12:16:26 -0400

    server receive ajax request

--- app.py
+++ app.py
@@ -1,21 +1,23 @@
 from flask import Flask,redirect, url_for, request, render_template
-# from keras.models import load_model
-# from keras.applications.resnet50 import ResNet50
-# from gevent.pywsgi import WSGIServer
+from keras.models import load_model
+from keras.preprocessing import image
+from keras.applications.resnet50 import ResNet50, decode_predictions
+
 
 app = Flask(__name__)
 
 
-
+file_path = "static/image/"
 
 MODEL_PATH='models/your_model.h5'
 
-# model = ResNet50(weights='imagenet')
+model = ResNet50(weights='imagenet')
 
 
 
 def model_predict (img_path, model):
 
+	print(img_path)
 	img = image.load_img(img_path)
 
 	x = image.img_to_array(img) 
@@ -26,20 +28,22 @@ def model_predict (img_path, model):
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
-
-@app.route('/')
-def hello():
-    return render_template('hello.html')
+	print('index')
+	return render_template('index.html')
 	
 @app.route('/predict',methods=['POST'])
 def upload():
-    if request.method == 'POST':
+	if request.method == 'POST':
+		print("\n\n\n\nHELLO WORLD\n\n\n\n")
+		f = request.files['file']
+		
+		fullpathname=file_path+f.name+'.jpg'
+		f.save(fullpathname)
+		preds = model_predict(fullpathname, model)
+		pred_class = decode_predictions(preds, top=1)
+		print("\n\n\n\n")
+		return pred_class
 
-    	f = request.files['file']
-    	f.save(file_path)
-    	preds = model_predict(file_path, model)
-    	pred_class = decode_prediction(preds, top=1)
-    	return pred_class
 
-
+if __name__ == "__main__":
+	app.run(debug=True)
--- static/js/main.js
+++ static/js/main.js
@@ -1,10 +1,8 @@
 $(document).ready(function(){
 
     function readURL(input) {
-        console.log('hey2')
         if (input.files && input.files[0]) {
             var reader = new FileReader();
-            console.log('hey3')
             reader.onload = function (e) {
                 $('#img')
                     .attr('src', e.target.result);
@@ -16,6 +14,29 @@ $(document).ready(function(){
     $('#imageUpload').change(function(){
         readURL(this)
     })
+    $('#button_Classify').click(function(event){
+        var fd = new FormData()
+        var files = $('#imageUpload')
+        var files1 = $('#imageUpload')[0]
+        var files2 = $('#imageUpload')[0].files[0]
+        console.log(files2)
+        fd.append('file',files2)
+        console.log(fd)
+        $.ajax ({
+            type:'POST',
+            url: '/predict',
+            data : fd,
+            contentType : false,
+            cache: false,
+            processData: false,
+            async: true,
+            success: function (data){
+                $('#prediction').text(data);
+                console.log('ajax works!')
+            }
 
+        })
+        
+    })
 });
 
